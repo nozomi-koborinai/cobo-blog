b9aa8846d738ff7af975febe45a3e38422dc79cc
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-12-28 14:25:13 +0900

    fix: ci

---
+++ __tests__/example.test.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest';
+
+describe('sample', () => {
+  it('test_1', () => {
+    expect(true).toBe(true);
+  });
+
+  it('test_2', () => {
+    expect(2 + 2).toBe(4);
+  });
+});

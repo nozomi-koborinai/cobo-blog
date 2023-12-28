e93333bf5c5af5c65b23f6d86d487bcc7bb5bfe0
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-12-28 11:45:28 +0900

    feat: init design

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <footer>
+        <p>&copy; 2023 cobo blog</p>
+      </footer>
+    );
+  }
+  
---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return (
+      <header>
+        <h1>cobo blog</h1>
+        <nav>
+          <ul>
+            <li><a href="#">home</a></li>
+            <li><a href="#">category</a></li>
+            <li><a href="#">contact</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+  

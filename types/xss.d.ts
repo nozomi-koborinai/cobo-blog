b158695ce70696dd21f76599a5f8fb713060b138
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-12-29 13:16:59 +0900

    feat: list

---
+++ types/xss.d.ts
@@ -0,0 +1,12 @@
+declare module 'xss' {
+    export interface IFilterXSSOptions {
+      whiteList?: {
+        [key: string]: string[];
+      };
+      stripIgnoreTag?: boolean;
+      stripIgnoreTagBody?: string[];
+    }
+  
+    export default function xss(html: string, options?: IFilterXSSOptions): string;
+  }
+  
